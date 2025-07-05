9c0dd016892b5330673bace6d2f7acc98b11dfa6
Tarik Stafford <tarikstafford@gmail.com>
2025-07-05 12:02:33 +0800

    +

--- padel-app/apps/api/migrations/versions/abc123456789_add_notification_and_score_models.py
+++ padel-app/apps/api/migrations/versions/abc123456789_add_notification_and_score_models.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'notification_score_models_fix'
+revision: str = 'abc123456789'
 down_revision: Union[str, None] = '00762c138e14'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
